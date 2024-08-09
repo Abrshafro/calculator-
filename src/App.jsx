7fe4b3080ea2ec33694e7f96a20f24983150b37a
Abrshafro <abrhammgetahun@gmail.com>
2024-08-09 13:01:14 -0700

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 
 import React,{useState} from "react"
 function App() {
-  const[result,setResult]= useState("1234");
+  const[result,setResult]= useState("");
+  const handleClick = (e) => {
+    setResult((prev) => prev.concat(e.target.id))
+  }
+  const clearAll = () =>setResult("");
+  const deleteOne = () => setResult(result.slice(0, -1));
+  const calculate = () =>{
+    try{
+      setResult(eval(result).toString())
+    }catch(error){
+      setResult("Error")
+    }
+  }
 
   return (
   <div className="min-h-screen w-screen flex flex-col items-center gap-10 px-3 py-10">
@@ -10,25 +22,25 @@ function App() {
       <input className="bg-slate-900/80 rounded-md p-4 w-full text-2xl font-semibold text-white text-right tracking-wider boorder-2 border-red-400 shadow-inner shadow-current" type="text" value={result} disabled/>
       <p className="bg-slate-300 inline-block rounded-full px-3 py-1 my-2 text-red-500 text-sm tracking wider border-slate-400">Efuye's Calculator</p>
       <div className="grid grid-cols-4">
-        <button type="button" className="operator">AC</button>
-        <button type="button" className="operator">DEL</button>
-        <button type="button" className="operator" id=".">.</button>
-        <button type="button" className="operator "id="/">/</button>
-        <button type="button" className="number" id="7">7</button>
-        <button type="button" className="number" id="8">8</button>
-        <button type="button" className="number" id="9">9</button>
-        <button type="button" className="operator "id="*">*</button>
-        <button type="button" className="number" id="4">4</button>
-        <button type="button" className="number" id="5">5</button>
-        <button type="button" className="number" id="6">6</button>
-        <button type="button" className="operator "id="-">-</button>
-        <button type="button" className="number" id="1">1</button>
-        <button type="button" className="number" id="2">2</button>
-        <button type="button" className="number" id="3">3</button>
-        <button type="button" className="operator "id="+">+</button>
-        <button type="button" className="number" id="00">00</button>
-        <button type="button" className="number" id="0">0</button>
-        <button type="button" className="operator col-span-2" id="=">=</button>
+        <button onClick={clearAll} type="button" className="operator">AC</button>
+        <button onClick={deleteOne} type="button" className="operator">DEL</button>
+        <button onClick={handleClick} type="button" className="operator" id=".">.</button>
+        <button onClick={handleClick} type="button" className="operator "id="/">/</button>
+        <button onClick={handleClick} type="button" className="number" id="7">7</button>
+        <button onClick={handleClick} type="button" className="number" id="8">8</button>
+        <button onClick={handleClick} type="button" className="number" id="9">9</button>
+        <button onClick={handleClick} type="button" className="operator "id="*">*</button>
+        <button onClick={handleClick} type="button" className="number" id="4">4</button>
+        <button onClick={handleClick} type="button" className="number" id="5">5</button>
+        <button onClick={handleClick} type="button" className="number" id="6">6</button>
+        <button onClick={handleClick} type="button" className="operator "id="-">-</button>
+        <button onClick={handleClick} type="button" className="number" id="1">1</button>
+        <button onClick={handleClick} type="button" className="number" id="2">2</button>
+        <button onClick={handleClick} type="button" className="number" id="3">3</button>
+        <button onClick={handleClick} type="button" className="operator "id="+">+</button>
+        <button onClick={handleClick} type="button" className="number" id="00">00</button>
+        <button onClick={handleClick} type="button" className="number" id="0">0</button>
+        <button onClick={calculate} type="button" className="operator col-span-2" id="=">=</button>
       </div>
     </div>
 
