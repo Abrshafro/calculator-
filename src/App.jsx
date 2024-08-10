d117e824fed571facbcba997f0875cb752637283
Abrshafro <abrhammgetahun@gmail.com>
2024-08-09 23:14:01 -0700

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   }
 
   return (
-  <div className="min-h-screen w-screen flex flex-col items-center gap-10 px-3 py-10">
+  <div className="min-h-screen w-screen flex flex-col items-center gap-10 px-3 py-10 bg-slate-200">
     <h1 className="text-4xl text-yellow-300 font-bold">Calculator</h1>
     <div className="bg-green-300 p-4 max-w-96 rounded-md border-yellow-300 shadow-blue-600">
       <input className="bg-slate-900/80 rounded-md p-4 w-full text-2xl font-semibold text-white text-right tracking-wider boorder-2 border-red-400 shadow-inner shadow-current" type="text" value={result} disabled/>
